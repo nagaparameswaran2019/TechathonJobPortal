f6a11722a813e60457e0e494f4755d8cb6883207
Nagaparameswaran Jayaram <Nagaparameswaran.Jayaram@cudirect.com>
2023-01-20 20:57:29 +0530

    Org sub type added in regirster page

--- UIJobPortal/src/pages/auth/Register.js
+++ UIJobPortal/src/pages/auth/Register.js
@@ -11,34 +11,58 @@ import {
 } from "../../components/Component";
 import Logo from "../../images/logo.png";
 import LogoDark from "../../images/logo-dark.png";
-import { Spinner, FormGroup } from "reactstrap";
+import { Spinner, FormGroup, Form } from "reactstrap";
 import PageContainer from "../../layout/page-container/PageContainer";
 import Head from "../../layout/head/Head";
 import AuthFooter from "./AuthFooter";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
+import { ComboBox } from "@progress/kendo-react-dropdowns";
 
 const Register = ({ history }) => {
+  var coreTypeDataSource = [{ CoreTypeID: 1, CoreType: "MCA" },
+  { CoreTypeID: 2, CoreType: "MBA" }, { CoreTypeID: 3, CoreType: "BCA" }];
+
   const [passState, setPassState] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [dataSource, setdataSource] = useState(coreTypeDataSource);
   const { errors, register, handleSubmit } = useForm();
+
+  const [value, setValue] = React.useState(null);
 
   const handleFormSubmit = () => {
     setLoading(true);
     setTimeout(() => history.push(`${process.env.PUBLIC_URL}/auth-success`), 2000);
   };
+
+  const handleChange = (event) => {
+    setValue(event.value);
+  };
+  
+  const onRadioButtonChanged = (event) => {
+    setValue("");
+    if (event.currentTarget.dataset.orgType == "Institution") {
+      setdataSource(coreTypeDataSource);
+    }
+    else {
+      var coreTypeDataSource1 = [{ CoreTypeID: 1, CoreType: "IT" },
+      { CoreTypeID: 2, CoreType: "ITES" }, { CoreTypeID: 3, CoreType: "Civil" }];
+      setdataSource(coreTypeDataSource1);
+    } 
+  };
+
   return (
     <React.Fragment>
       <Head title="Register" />
       <PageContainer>
         <Block className="nk-block-middle nk-auth-body  wide-xs">
           <div className="brand-logo pb-4 text-center">
-          <h2 tag="h4">Job Portal</h2>
+            <h2 tag="h4">Job Portal</h2>
           </div>
           <PreviewCard className="card-bordered" bodyClass="card-inner-lg">
             <BlockHead>
               <BlockContent>
-                <h4>Register</h4> 
+                <h4>Register</h4>
               </BlockContent>
             </BlockHead>
             <form className="is-alter" onSubmit={handleSubmit(handleFormSubmit)}>
@@ -79,8 +103,50 @@ const Register = ({ history }) => {
               </FormGroup>
               <FormGroup>
                 <div className="form-label-group">
+                  <label className="form-label" htmlFor="default-01">
+                    Organization Type
+                  </label>
+                </div>
+                <div className="form-control-wrap">
+                  <div className="custom-control custom-control-sm custom-radio">
+                    <input type="radio" className="custom-control-input"
+                      defaultChecked={true}
+                      name="radio-list"
+                      id="radio-bl"
+                      data-org-type="Institution"
+                      onChange={onRadioButtonChanged} />
+                    <label className="custom-control-label" htmlFor="radio-bl">Institution</label>
+                  </div>
+                  <div className="custom-control custom-control-sm custom-radio" style={{ marginLeft: 10 }}>
+                    <input type="radio" className="custom-control-input" data-org-type="Company" name="radio-list" id="radio-ph" onChange={onRadioButtonChanged} />
+                    <label className="custom-control-label" htmlFor="radio-ph">Company</label>
+                  </div>
+                </div>
+              </FormGroup>
+              <FormGroup>
+                <div className="form-label-group">
+                  <label className="form-label" htmlFor="default-033">
+                    Organization Core Type
+                  </label>
+                </div>
+                <div className="form-control-wrap">
+                  <ComboBox
+                    data={dataSource}
+                    textField="CoreType"
+                    dataItemKey="CoreTypeID"
+                    value={value}
+                    onChange={handleChange}
+                    placeholder="Please select ..."
+                    style={{
+                      width: "300px",
+                    }}
+                  />
+                </div>
+              </FormGroup>
+              <FormGroup>
+                <div className="form-label-group">
                   <label className="form-label" htmlFor="password">
-                    Passcode
+                    Password
                   </label>
                 </div>
                 <div className="form-control-wrap">
@@ -101,7 +167,37 @@ const Register = ({ history }) => {
                     id="password"
                     name="passcode"
                     ref={register({ required: "This field is required" })}
-                    placeholder="Enter your passcode"
+                    placeholder="Enter your password"
+                    className={`form-control-lg form-control ${passState ? "is-hidden" : "is-shown"}`}
+                  />
+                  {errors.passcode && <span className="invalid">{errors.passcode.message}</span>}
+                </div>
+              </FormGroup>
+              <FormGroup>
+                <div className="form-label-group">
+                  <label className="form-label" htmlFor="password">
+                    Confirm Password
+                  </label>
+                </div>
+                <div className="form-control-wrap">
+                  <a
+                    href="#password"
+                    onClick={(ev) => {
+                      ev.preventDefault();
+                      setPassState(!passState);
+                    }}
+                    className={`form-icon lg form-icon-right passcode-switch ${passState ? "is-hidden" : "is-shown"}`}
+                  >
+                    <Icon name="eye" className="passcode-icon icon-show"></Icon>
+
+                    <Icon name="eye-off" className="passcode-icon icon-hide"></Icon>
+                  </a>
+                  <input
+                    type={passState ? "text" : "password"}
+                    id="confirmPassword"
+                    name="confirmPassword"
+                    ref={register({ required: "This field is required" })}
+                    placeholder="Enter your confirm password"
                     className={`form-control-lg form-control ${passState ? "is-hidden" : "is-shown"}`}
                   />
                   {errors.passcode && <span className="invalid">{errors.passcode.message}</span>}
--- UIJobPortal/src/pages/auth/Success.js
+++ UIJobPortal/src/pages/auth/Success.js
@@ -12,11 +12,10 @@ const Success = () => {
     <React.Fragment>
       <Head title="Success" />
       <PageContainer>
-        <Block className="nk-block-middle nk-auth-body">
+        <Block className="nk-block-middle nk-auth-body  text-center">
           <div className="brand-logo pb-5">
             <Link to={process.env.PUBLIC_URL + "/"} className="logo-link">
-              <img className="logo-light logo-img logo-img-lg" src={Logo} alt="logo" />
-              <img className="logo-dark logo-img logo-img-lg" src={LogoDark} alt="logo-dark" />
+              <h2>Job Portal</h2>
             </Link>
           </div>
           <BlockHead>
